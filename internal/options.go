eb668d3be371326cd6849fee0ff75fa74023d3fa
Kouame Behouba Manasse <behouba@gmail.com>
2024-07-20 21:53:36 -0400

    memparse: add search functionnality to memparse
    This commit adds `--search`, `--search-regex` and `--context` flags
    to `memparse`, enabling pattern search in a process's memory pages.
    
    Signed-off-by: Kouame Behouba Manasse <behouba@gmail.com>

--- internal/options.go
+++ internal/options.go
@@ -1,15 +1,18 @@
 package internal
 
 var (
-	Format         string
-	Stats          bool
-	Mounts         bool
-	OutputFilePath string
-	PID            uint32
-	PsTree         bool
-	PsTreeCmd      bool
-	PsTreeEnv      bool
-	Files          bool
-	Sockets        bool
-	ShowAll        bool
+	Format             string
+	Stats              bool
+	Mounts             bool
+	OutputFilePath     string
+	PID                uint32
+	PsTree             bool
+	PsTreeCmd          bool
+	PsTreeEnv          bool
+	Files              bool
+	Sockets            bool
+	ShowAll            bool
+	SearchPattern      string
+	SearchRegexPattern string
+	SearchContext      int
 )
